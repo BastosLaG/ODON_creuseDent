9771d25746d75cf6789b9c652bd44a1f45a2373e
Bastien CAPIAUX <bastien.capiaux@gmail.com>
2025-02-19 17:00:36 +0100

    Update 🍎 Change the teeth for choosing the good sketch

--- Assets/HighlightsTeethManager.cs
+++ Assets/HighlightsTeethManager.cs
@@ -1,12 +1,13 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
-using System.Collections.Generic;
 
 public class HighlightsTeethManager : MonoBehaviour
 {
-    [SerializeField] private GameObject[] teethSphere;
-    [SerializeField] private Dictionary<stateTheeth, List<GameObject>> teethGroups = new();
-
-    private enum stateTheeth
+    [SerializeField] private TeethStruct[] teethStructList;
+    [SerializeField] private int currentState;
+    [SerializeField] private int maxState;
+    public enum stateTheeth
     {
         UPPERRIGHT,
         UPPERLEFT,
@@ -14,64 +15,100 @@ public class HighlightsTeethManager : MonoBehaviour
         LOWERRIGHT
     };
 
+    [System.Serializable]
+    public struct TeethStruct
+    {
+        public stateTheeth state;
+        public GameObject tooth;
+        public int index;
+    }
+
     void Start()
     {
+        currentState = 0;
+        maxState = 3;
         InitializeTeeth();
-        SortTeeth();
         CleanTeeth();
+        switchState(0);
     }
+
 
     void InitializeTeeth()
     {
         Transform[] children = GetComponentsInChildren<Transform>();
-        teethSphere = new GameObject[children.Length - 1];
+
+        // Initialiser la taille du tableau teethStructList
+        teethStructList = new TeethStruct[children.Length - 1];
 
         int i = 0;
         foreach (Transform child in children)
         {
             if (child != transform)
             {
-                teethSphere[i] = child.gameObject;
+                TeethStruct toothStruct = new TeethStruct();
+                toothStruct.tooth = child.gameObject;
+                toothStruct.index = i%8+1;
+
+                if (i <= 7)
+                {
+                    toothStruct.state = stateTheeth.UPPERRIGHT;
+                }
+                else if (i > 7 && i <= 15)
+                {
+                    toothStruct.state = stateTheeth.UPPERLEFT;
+                }
+                else if (i > 15 && i <= 23)
+                {
+                    toothStruct.state = stateTheeth.LOWERLEFT;
+                }
+                else if (i > 23 && i <= 31)
+                {
+                    toothStruct.state = stateTheeth.LOWERRIGHT;
+                }
+
+                teethStructList[i] = toothStruct;
                 i++;
             }
         }
-
-        // Initialiser les groupes
-        foreach (stateTheeth state in System.Enum.GetValues(typeof(stateTheeth)))
-        {
-            teethGroups[state] = new List<GameObject>();
-        }
     }
-
-    void SortTeeth()
+    void SetTeeth(stateTheeth state, int[] indexList)
     {
-        int i = 0;
-        foreach (GameObject tooth in teethSphere)
+        foreach (TeethStruct item in teethStructList)
         {
-            if (i <= 7 ) {
-                teethGroups[stateTheeth.UPPERRIGHT].Add(tooth);
-            } else if (i > 7 && i <= 15) {
-                teethGroups[stateTheeth.UPPERLEFT].Add(tooth); 
-            } else if (i > 15 && i <= 23){
-                teethGroups[stateTheeth.LOWERLEFT].Add(tooth);
-            } else if (i > 23 && i <= 31) {
-                teethGroups[stateTheeth.LOWERRIGHT].Add(tooth);
+            if (item.state == state)
+            {
+                foreach (int index in indexList)
+                {
+                    if (item.index == index)
+                    {
+                        item.tooth.SetActive(true);
+                    }
+                }
             }
-            i++;
         }
+}
 
-        // Vérification dans la console
-        foreach (var group in teethGroups)
-        {
-            Debug.Log(group.Key + " : " + group.Value.Count + " teeth");
+    public void switchState(int increment){
+        currentState = (currentState+increment)%maxState;
+        CleanTeeth();
+        switch (currentState) {
+            case 0:
+                SetTeeth(stateTheeth.LOWERRIGHT, new int[] { 7 });
+                break;    
+            case 1:
+                SetTeeth(stateTheeth.LOWERRIGHT, new int[] { 3 });
+                break;
+            case 2:
+                SetTeeth(stateTheeth.LOWERRIGHT, new int[] { 1 });          
+                break;
         }
     }
 
     void CleanTeeth()
     {
-        foreach (GameObject tooth in teethSphere)
+        foreach (TeethStruct tooth in teethStructList)
         {
-            tooth.SetActive(false);
+            tooth.tooth.SetActive(false);
         }
     }
 }
