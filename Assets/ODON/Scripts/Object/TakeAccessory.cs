85e7817b628b48dd809661d76a2828db669d3c2d
Dragone65 <florian.bruguier65@outlook.fr>
2025-03-05 17:32:47 +0100

    Update 🍎 TakeAccessory et lunettes

--- Assets/ODON/Scripts/Object/TakeAccessory.cs
+++ Assets/ODON/Scripts/Object/TakeAccessory.cs
@@ -2,45 +2,22 @@ using UnityEngine;
 
 public class TakeAccessory : MonoBehaviour
 {
-    [Header("Options d'équipement")]
-    public bool isGloves = false;
 
     [Header("Cible par défaut")]
     public GameObject target;
     public Transform localisedTarget;
 
-    [Header("Équipement spécifique pour les gants")]
-    public GameObject leftHand;
-    public GameObject rightHand;
-    public GameObject leftglovePrefab;
-    public GameObject rightglovePrefab;
-    public Transform leftHandTarget;
-    public Transform rightHandTarget;
 
     private bool isEquipped = false;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (isEquipped) return; 
+        Debug.Log("Collision détectée avec : " + other.name);
 
-        if (!isGloves && other.CompareTag("Player"))
+        if (LayerMask.NameToLayer("Object") == other.gameObject.layer)
         {
+            Debug.Log("dans la main");
             AttachToTarget(target.transform, localisedTarget);
-            DestroyObject(localisedTarget);
-        }
-        else if (isGloves)
-        {
-            if (other.name == "RightHand" && rightHandTarget != null)
-            {
-                EquipGlove(true, rightHandTarget);
-                DestroyObject(rightglovePrefab);
-            }
-            else if (other.name == "LeftHand" && leftHandTarget != null)
-            {
-                EquipGlove(false, leftHandTarget);
-                DestroyObject(leftglovePrefab);
-
-            }
         }
     }
 
@@ -52,16 +29,6 @@ public class TakeAccessory : MonoBehaviour
         transform.localRotation = Quaternion.identity;
         Destroy(GetComponent<Rigidbody>());
         Destroy(GetComponent<Collider>());
-        isEquipped = true;
-    }
-
-    private void EquipGlove(bool isRightHand, Transform handTarget)
-    {
-        Debug.Log("le gant est bien attaché au joueur");
-        GameObject glovePrefab = isRightHand ? rightglovePrefab : leftglovePrefab;
-        GameObject glove = Instantiate(glovePrefab, handTarget);
-        glove.transform.localPosition = Vector3.zero;
-        glove.transform.localRotation = Quaternion.identity;
         isEquipped = true;
     }
 }
