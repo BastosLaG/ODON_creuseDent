195011fede253b16e6e4b4a53c6d8b5ed4b2b53c
Bastien CAPIAUX <bastien.capiaux@gmail.com>
2025-06-02 13:13:57 +0200

    ✨ Add SceneNames class for centralized scene name management and refactor UISceneLoader to use it

---
+++ Assets/ODON/Scripts/Data/SceneNames.cs
@@ -0,0 +1,9 @@
+namespace ODON.Data
+{
+    public static class SceneNames
+    {
+        public const string Game = "ODON1";
+        public const string Tutorial = "Tutorial";
+        public const string MainMenu = "MainMenu";
+    }
+}
--- Assets/ODON/Scripts/UI/UISceneLoader.cs
+++ Assets/ODON/Scripts/UI/UISceneLoader.cs
@@ -4,81 +4,87 @@ using UnityEngine.UI;
 using System.Collections;
 using TMPro;
 using Unity.VisualScripting;
+using Unity.Android.Gradle.Manifest;
 
-public class SceneLoader : MonoBehaviour
+namespace ODON.UI
 {
-    public GameObject loadingScreen;
-    public Image progressBar;
-    public TextMeshProUGUI progressText;
-
-    public Toggle m_Toggle;
-
-    private string sceneGameName = "ODON1";
-    private string sceneTutorialName = "Tutorial";
-
-    private bool isTutorial = false;
-    public string sceneName
+    public class SceneLoader : MonoBehaviour
     {
-        get { return isTutorial ? sceneTutorialName : sceneGameName; }
-        set { sceneGameName = value; }
-    }
+        public GameObject loadingScreen;
+        public Image progressBar;
+        public TextMeshProUGUI progressText;
 
-    void Start()
-    {
-        if (m_Toggle != null)
+        public Toggle m_Toggle;
+
+        private string sceneGameName = Data.SceneNames.Game;
+        private string sceneTutorialName = Data.SceneNames.Tutorial;
+
+        private bool isTutorial = false;
+        public string SceneName
         {
-            m_Toggle.onValueChanged.AddListener(delegate
-            {
-                ToggleValueChanged(m_Toggle);
-            });
+            get { return isTutorial ? sceneTutorialName : sceneGameName; }
+            set { sceneGameName = value; }
         }
-        else
+
+        void Start()
         {
-            Debug.LogWarning("Toggle is not assigned in SceneLoader.");
-        }
-    }
-
-
-    public void LoadScene()
-    {
-        StartCoroutine(LoadSceneAsync(sceneName));
-    }
-
-    public void LoadScene(string sceneName)
-    {
-        this.sceneName = sceneName;
-        StartCoroutine(LoadSceneAsync(sceneName));
-    }
-
-    IEnumerator LoadSceneAsync(string sceneName)
-    {
-        if (loadingScreen != null)
-            loadingScreen.SetActive(true);
-
-        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
-        operation.allowSceneActivation = false;
-
-        while (!operation.isDone)
-        {
-            float progress = Mathf.Clamp01(operation.progress / 0.9f);
-
-            if (progressBar != null)
-                progressBar.fillAmount = progress;
-
-            if (progressText != null)
-                progressText.text = "Chargement en cours... " + Mathf.RoundToInt(progress * 100f) + "%";
-            if (operation.progress >= 0.9f)
+            if (m_Toggle != null)
             {
-                yield return new WaitForSeconds(0.5f);
-                operation.allowSceneActivation = true;
+                m_Toggle.onValueChanged.AddListener(delegate
+                {
+                    ToggleValueChanged(m_Toggle);
+                });
             }
+            else
+            {
+                isTutorial = false;
+                Debug.LogWarning("Toggle is not assigned in SceneLoader.");
+            }
+        }
 
-            yield return null;
+
+        public void LoadScene()
+        {
+            StartCoroutine(LoadSceneAsync(SceneName));
+        }
+
+        public void LoadScene(string sceneName)
+        {
+            SceneName = sceneName;
+            StartCoroutine(LoadSceneAsync(SceneName));
+        }
+
+        IEnumerator LoadSceneAsync(string sceneName)
+        {
+            if (loadingScreen != null)
+                loadingScreen.SetActive(true);
+
+            AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+            operation.allowSceneActivation = false;
+
+            while (!operation.isDone)
+            {
+                float progress = Mathf.Clamp01(operation.progress / 0.9f);
+
+                if (progressBar != null)
+                    progressBar.fillAmount = progress;
+
+                if (progressText != null)
+                    progressText.text = "Chargement en cours... " + Mathf.RoundToInt(progress * 100f) + "%";
+                if (operation.progress >= 0.9f)
+                {
+                    yield return new WaitForSeconds(0.5f);
+                    operation.allowSceneActivation = true;
+                }
+
+                yield return null;
+            }
+        }
+
+        public void ToggleValueChanged(Toggle change)
+        {
+            isTutorial = change.isOn;
         }
     }
-    
-    public void ToggleValueChanged(Toggle change)
-    {
-        isTutorial = change.isOn;
-    }
+
 }
