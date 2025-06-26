e481ef01d0a8899a0807e43ef4d43520baa81d32
BastosLaG <bastien.capiaux@gmail.com>
2025-06-26 17:57:32 +0200

    Refactor interactable object scripts: start of previewPutElement
    - Deleted the DiguePreview script which managed the preview of the Digue object.
    - Introduced the PreviewPutComponant script to handle the placement of components with improved functionality.
    - Updated the scene files to reflect changes in object references and icons.
    - Adjusted the material handling and interaction logic in the new script.

--- Assets/ODON/Scripts/InteractableObject/PreviewPutComponant.cs
+++ Assets/ODON/Scripts/InteractableObject/PreviewPutComponant.cs
@@ -1,15 +1,18 @@
-using UnityEditor.SceneManagement;
 using UnityEngine;
 
 namespace ODON
 {
-    public class DiguePreview : MonoBehaviour
+    public class PreviewPutComponant : MonoBehaviour
     {
         [SerializeField] private Material baseMaterial;
         [SerializeField] private Material materialPreview;
         [SerializeField] private bool isContact = false;
-
+        [SerializeField] private GameObject originalObject;
         [SerializeField] private Renderer rd;
+
+        [SerializeField] private UniversalSenderActionToEventManager USATEManager;
+
+        [SerializeField] private LayerMask layerToFind;
 
         void Start()
         {
@@ -19,19 +22,25 @@ namespace ODON
             rd.enabled = false;
         }
 
-        void PlaceDigue()
+        public void PlaceDigue()
         {
             if (isContact)
             {
                 rd.enabled = true;
                 rd.material = baseMaterial;
                 Destroy(this);
+                Destroy(originalObject);
+                USATEManager.TryValdidateCurrentItem();
+            }
+            else
+            {
+                USATEManager.TryValdidateCurrentItem(false);
             }
         }
 
         void OnTriggerEnter(Collider other)
         {
-            if (other.gameObject.layer == LayerMask.NameToLayer("Dam"))
+            if (other.gameObject.layer == layerToFind)
             {
                 rd.enabled = true;
                 isContact = true;
@@ -40,7 +49,7 @@ namespace ODON
 
         void OnTriggerExit(Collider other)
         {
-            if (other.gameObject.layer == LayerMask.NameToLayer("Dam"))
+            if (other.gameObject.layer == layerToFind)
             {
                 rd.enabled = false;
                 isContact = false;
